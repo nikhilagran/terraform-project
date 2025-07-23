18074bee3778d6640884eba56f03ae573bcb21c2
nikhilagran <97845636+nikhilagran@users.noreply.github.com>
2025-07-23 18:42:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,30 @@
-provider "aws" {
-  region = var.region
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = var.bucket_name
+provider "local" {}
+provider "null" {}
+
+# Create local directory
+resource "null_resource" "make_directory" {
+  provisioner "local-exec" {
+    command = "mkdir -p ${var.folder_name}"
+  }
+}
+
+# Create a local file inside the directory
+resource "local_file" "example_file" {
+  depends_on = [null_resource.make_directory]
+
+  content  = var.file_content
+  filename = "${var.folder_name}/${var.file_name}"
 }
